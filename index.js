3fd15a54164d56ef8c1710f56582353b8dcdbb2e
RafaelGSS <rafael.nunu@hotmail.com>
2021-03-27 17:59:18 -0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Fastify = require('fastify')
+const fastify = Fastify({ logger: true })
+
+async function slowPromise() {
+  const rand = Math.random() * 1000
+  await new Promise((resolve) => setTimeout(resolve, rand))
+  return true
+}
+
+fastify.get('/', (req, reply) => {
+  reply.send({ ok: true })
+})
+
+fastify.get('/slow', async (req, reply) => {
+  const result = await slowPromise()
+  return { ok: result }
+})
+
+fastify.listen(3000, '0.0.0.0', (err) => {
+  if (err) throw err
+})
