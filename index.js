c73eadb2989e7d9ba79056b93da6862e30531ada
RafaelGSS <rafael.nunu@hotmail.com>
2021-03-29 09:52:07 -0300

    update: logger only if node_env development

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Fastify = require('fastify')
-const fastify = Fastify({ logger: true })
+const fastify = Fastify({ logger: NODE_ENV === 'development' })
 
 async function slowPromise() {
   const rand = Math.random() * 1000
